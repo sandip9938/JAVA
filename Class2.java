0c16af2a4b66009863e42ce0b433382e78a45f56
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-10-21 21:04:24 +0530

    Class2

---
+++ Class2.java
@@ -0,0 +1,19 @@
+public class Class2 {
+    public static void main(String[] args) {
+        // ex-1
+        int x=10;
+        int y=010;
+        int z=0X10;
+        System.out.println(x+"---"+y+"---"+z);
+        //ex-2
+          int xl=10;
+       long yl=10l;
+      long zl=100l;
+        System.out.println(xl+"---"+yl+"---"+zl);
+        // ex-3 
+        double d = 456789.123456789;
+        float f = (float)d;
+        System.out.println("double to float : "+f);
+
+    }
+}
