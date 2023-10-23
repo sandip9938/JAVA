218293335a32cb042a09d627984bb2e906ad424a
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-10-23 21:06:10 +0530

    A1

---
+++ Class4.java
@@ -0,0 +1,72 @@
+public class Class4 {
+    public static void main(String[] args) {
+        // public- acess modifier
+        // static- allows to call this method without a object of class
+        // void- return type
+        // main- function name
+        // String[]args- Command line argummments
+        System.out.print("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("hii friends ");
+        // System-deafult class
+        // out- object which is belongs to printstream
+        // print - all print in one line
+        // println - allare print in one by one line
+
+        // -------------------java Varibale-----------------------------
+        // variable- its use to store a data
+        // ex=
+        int money = 1000;
+        System.out.println(money);
+        // int - its a datatypes
+        // money - its a varible which is store 1000
+        // 1000 - its a data
+
+        // ------------------JAVA NAMING CONVENTION ---------------------------
+        // Lowercase- all are letter is lower case
+        String name = "sandip";
+        System.out.println(name.toLowerCase());
+        // Uppercase- all are letter is Upper case
+        System.out.println(name.toUpperCase());
+        // CamelCase- first letter is upper and other letters are lower
+        System.out.println(name);
+        // PascalCase- each word's first letter is capitalized except the first word
+        // Snake_case- underscore (_) between words
+        // kebab-case- hyphen (-) between words
+
+        // ---------------------------JAVA INDETIFIER----------------------------------
+        // A valid identifier must start with a letter (a-z, A-Z), an underscore (_), or
+        // a dollar sign ($)
+        // It can contain only letters, digits, and underscores
+        // Identifiers cannot begin with two consecutive underscores
+        // Identifiers cannot be reserved keywords
+        // ITS case sensative
+        // WhiteSpace not permited
+
+        // ------------------------------JAVA DATATYPES
+        // --------------------------------------------------
+
+        // Primitive Data Types:
+        // byte : 8 bits : 1byte
+        // short : 16 bits :2byte
+        // int : 32 bits : 4byte
+        // long : 64 bits : 8byte
+        // float : 32.334 : 32bits : 4byte
+        // boolean : true : NA
+        // char : 'a' :16bits : 2byte
+        // double : 345678.876432 : 64bits : 8byte
+
+        // Non-Primitive Data Types:
+        // 1.String
+        // 2.Array
+        // 3.Object
+        // 4.Class
+        // 5.Interface
+        // 6.Enum
+        // 7.Exception
+
+
+        
+
+    }
+}
