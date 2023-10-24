13aa57e8e896addd65107597e8c862b96bd19c41
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-10-24 21:20:15 +0530

    Input

---
+++ Class5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Class5 {
+    public static void main(String[] args) {
+        //-------------------TAKING INPUT-----------------------------
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Please types x :");
+        int x = sc.nextInt();
+        System.out.println("Enter the number of elements you want in your array:" +x);
+        // Scanner - its a class in java package .util library
+        //sc - variables name
+        // new - new is a class
+        // System.in -  its system input
+
+
+        // ---------------------Scanner Method ---------------------------------
+        // next() - returns string value from user
+        // nextDouble() - returns double value from user
+        // nextFloat() - returns float value from user
+        // nextByte() - returns byte value from user
+        // nextShort() - returns short value from user
+        // nextLong() - returns long value from user
+        // nextBoolean() - returns boolean value from user
+        // nextLine() - returns line value from user
+        // nextInt() - returns integer value from user
+        
+
+
+
+    }
+}
