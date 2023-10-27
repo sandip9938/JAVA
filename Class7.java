367db2995406656f9be3a00f375d12d4e1786fbf
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-10-27 13:17:38 +0530

    Class 7

--- Class7.java
+++ Class7.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Class7 {
     public static void main(String[] args) {
-        
+       sum();
+    }
+    static void sum(){
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int num1=sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int num2=sc.nextInt();
+        System.out.println("all rows number :" +num1);
+        System.out.println("all column number :"+num2);
     }
 }
