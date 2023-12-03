77b5b72ec0c29b26a7f74de7f07bc2a33f3199c1
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-12-03 15:40:06 +0530

    Firstjava

---
+++ Ratan Java/Firstjava.java
@@ -0,0 +1,13 @@
+public class Firstjava {
+    public static void main(String[] args) {
+        //First java Programme
+        System.out.println("Hello");
+        System.out.println("hii \"Sandip\"panda");
+        System.out.println("hello\n sandip");
+        //Print()=Its print only one line
+        //Println() = its print line by line.
+
+
+
+    }
+}
