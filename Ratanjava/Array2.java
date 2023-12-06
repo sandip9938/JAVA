294e7f9cf93d1b67e50d2564b142b74ff65b94ef
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-12-06 12:14:49 +0530

    Firstjava update

--- Ratanjava/Array2.java
+++ Ratanjava/Array2.java
@@ -1,8 +1,66 @@
 package Ratanjava;
 
+import java.util.Scanner;
+
+//Arrayclass2
+
 public class Array2 {
     public static void main(String[] args) {
-        System.out.println("Arrayclass2");
+
+        //twonum();
+        //greet();
+        //sumtwo();
+      /*
+        int a=12;
+      int b=90;
+        swap( a ,  b);
+        System.out.println(a + " " +b);
+
+       */
+    }
+
+
+//Stack
+      //  its gives a refernce
+// Heap
+       // its give a actuall value.
+
+
+
+        //Add two number by input by function
+        static void  twonum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pls give num1 : ");
+        int num1=sc. nextInt();
+        System.out.println("Pls give num2 : ");
+        int num2=sc. nextInt();
+        int  twonum=num1+num2;
+        System.out.println("total number is  : " +twonum  );
+    }
+    // Greeting Function
+static String greet(){
+String greeting="Happy new Year";
+    System.out.println(greeting);
+    return greeting;
+}
+    //Add two number by input by function using a, b
+static int  sumtwo( ) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Pls give a : ");
+    int a = sc.nextInt();
+    System.out.println("Pls give b: ");
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println("total number is  : " + sum);
+return sum;
+}
+
+static void swap(int a, int b){
+        int temp=a;
+        a=b;
+        b=temp;
+
 
     }
+
 }
