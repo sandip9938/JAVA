db531780dd50dfdc848278c8293df64eeefc89c5
sandip9938 <sandippanda577@gmail.com>
2024-01-01 10:39:15 +0530

    1Jan

---
+++ JAVA/v7.java
@@ -0,0 +1,11 @@
+public class v7 {
+    public static void main(String[] args) {
+        System.out.println("Class-7");
+        //Exercise -1
+        System.out.println("*"); 
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
---
+++ JAVA/v8.java
@@ -0,0 +1,5 @@
+public class v8 {
+    public static void main(String[] args) {
+
+    }
+}
